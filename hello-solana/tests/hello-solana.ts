45201b96681878dad310af0ac6c8521e5abb14ae
Adarshk18 <adarshkohli183@gmail.com>
2025-09-14 17:30:10 +0530

    new

--- hello-solana/tests/hello-solana.ts
+++ hello-solana/tests/hello-solana.ts
@@ -1,16 +1,37 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { HelloSolana } from "../target/types/hello_solana";
+import { LAMPORTS_PER_SOL, SystemProgram, Transaction, sendAndConfirmTransaction, Keypair, Connection } from "@solana/web3.js";
 
-describe("hello-solana", () => {
-    // Configure the client to use the local cluster.
-    anchor.setProvider(anchor.AnchorProvider.env());
+const connection = new Connection("http://localhost:8899", "confirmed");
 
-    const program = anchor.workspace.HelloSolana as Program<HelloSolana>;
+const sender = new Keypair();
+const receiver = new Keypair();
 
-    it("Is initialized!", async () => {
-        // Add your test here.
-        const tx = await program.methods.initialize().rpc();
-        console.log("Your transaction signature", tx);
-    });
-});
+const signature = await connection.requestAirdrop(
+    sender.publicKey,
+    LAMPORTS_PER_SOL
+);
+
+await connection.confirmTransaction(signature, "confirmed");
+
+const transferInstruction = SystemProgram.transfer(
+    {
+        fromPubkey: sender.publicKey,
+        toPubkey: receiver.publicKey,
+        lamports: 0.01 * LAMPORTS_PER_SOL
+    }
+)
+
+const transaction = new Transaction().add(transferInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [sender]
+)
+
+console.log("Transaction Signature:", `${transactionSignature}`);
+
+const senderBalance = await connection.getBalance(sender.publicKey);
+const receiverBalance = await connection.getBalance(receiver.publicKey);
+
+console.log("Sender Balance:", `${senderBalance}`);
+console.log("Receiver Balance:", `${receiverBalance}`);
