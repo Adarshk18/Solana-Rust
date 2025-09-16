55c5e67108b066858a92d7063f37059cb31acbdd
Adarshk18 <adarshkohli183@gmail.com>
2025-09-16 19:56:05 +0530

    new

--- hello-solana/programs/hello-solana/src/lib.rs
+++ hello-solana/programs/hello-solana/src/lib.rs
@@ -35,15 +35,24 @@ declare_id!("4d7Grr2mQuPcVoASrf3k91LuRGk21LVFbsjWL1L5p3W5");
 pub mod pda{
     use super::*;
 
-    pub fn create(_ctx: Context<Create>) -> Result<()>{
+    pub fn create(ctx: Context<Create>, message: String) -> Result<()>{
+        msg!("Create Message: {}", message);
+        let account_data = &mut ctx.accounts.message_account;
+        account_data.user = ctx.accounts.user.key();
+        account_data.message = message;
+        account_data.bump = ctx.bumps.message_account;
         Ok(())
     }
 
-    pub fn update(_ctx: Context<Update>)->Result<()>{
+    pub fn update(ctx: Context<Update>, message: String)->Result<()>{
+        msg!("Update Message: {}", message);
+        let account_data = &mut ctx.accounts.message_account;
+        account_data.message = message;
         Ok(())
     }
 
-    pub fn delete(_ctx: Context<Delete>)-> Result<()>{
+    pub fn delete(_ctx: Context<Delete>, message: String)-> Result<()>{
+        msg!("Delete Message");
         Ok(())
 
     }
@@ -68,10 +77,35 @@ pub struct Create<'info>{
 }
 
 #[derive(Accounts)]
-pub struct Update {}
+#[instruction(message: String)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"message",user.key().as_ref()],
+        bump = message_account.bump,
+        realloc = 8+32+4+message.len()+1,
+        realloc::payer = user,
+        realloc::zero = true,
+    )]
+
+    pub message_account: Account<'info, MessageAccount>,
+    pub system_program: Program<'info,System>,
+}
 
 #[derive(Accounts)]
-pub struct Delete {}
+pub struct Delete<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"message", user.key().as_ref()],
+        bump = message_account.bump,
+        close = user,
+    )]
+    pub message_account: Account<'info, MessageAccount>,
+}
 
 #[account]
 pub struct MessageAccount {
